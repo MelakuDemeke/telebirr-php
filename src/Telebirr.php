2527b6ca576d562fd98297ad9e0e9ad3f53bca8f
MelakuDemeke <melaku.demeke789@gmail.com>
2022-12-16 15:40:41 +0300

    doc added

---
+++ src/Telebirr.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Melaku\Telebirr;
+
+/**
+ * Telebirr payment helper
+ * 
+ * @publicKey		public key provided form tele
+ * @appKey			app key provided form tele
+ * @appId			app id provided form tele
+ * @api				payment getway provided form tele
+ * @notifyUrl		your notify url which will get the after payment data
+ * @returnUrl		your sucess page
+ * @shortCode		short code form tele
+ * @timeoutExpress  pyament timeout usually it is 30s 
+ * @receiveName		the company name whos goingto recive the payment 
+ * @totalAmount		the amount shuld be paid
+ * @subject			payment subject
+ * 	
+ */
+
+
+?>
